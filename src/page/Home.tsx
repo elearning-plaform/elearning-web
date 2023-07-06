38a9cd9cbfca7ebf9fda899bcf3ed44e7def9e3d
William S <will.step.205@gmail.com>
2023-07-06 16:59:46 -0700

    import to TS

---
+++ src/page/Home.tsx
@@ -0,0 +1,45 @@
+import { useEffect } from 'react';
+import { signOut } from "firebase/auth";
+import { auth } from '../firebase';
+import { useNavigate } from 'react-router-dom';
+
+const Home = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            localStorage.removeItem('token');
+            navigate('/login', { replace: true });
+        }
+    }, [navigate])
+
+    const handleLogout = () => {
+        signOut(auth).then(() => {
+            // Sign-out successful.
+            localStorage.removeItem('token');
+            navigate("/login");
+            console.log("Signed out successfully")
+        }).catch((error) => {
+            // An error happened.
+        });
+    }
+
+    return (
+        <div>
+            <nav>
+                <p>
+                    Welcome Home
+                </p>
+
+                <div>
+                    <button onClick={handleLogout}>
+                        Logout
+                    </button>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Home;
