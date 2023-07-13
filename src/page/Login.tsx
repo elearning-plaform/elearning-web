7e05b45bdf0f938ec3122ecee2d22af636af710a
William S <will.step.205@gmail.com>
2023-07-13 13:24:32 -0700

    create your profile popup

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -1,9 +1,6 @@
 import '../assets/sass/Login.scss'
 import { useState } from 'react'
-import { 
-    signInWithEmailAndPassword, 
-    // signInAnonymously 
-} from 'firebase/auth'
+import { signInWithEmailAndPassword, } from 'firebase/auth'
 import { auth } from '../firebase'
 import { NavLink, useNavigate } from 'react-router-dom'
 import toast, { Toaster } from 'react-hot-toast';
@@ -77,32 +74,6 @@ const Login = () => {
 
     }
 
-    // function handleClickAsGuest(event: any) {
-    //     console.log("Login as Guest")
-
-    //     signInAnonymously(auth)
-    //         .then((userCredential) => {
-    //             // The user is signed in anonymously
-    //             // Retrieve the user's token
-    //             const user = userCredential.user;
-    //             user.getIdToken()
-    //                 .then((token) => {
-    //                     // Token retrieved successfully
-    //                     // navigate to home page
-    //                     localStorage.setItem('token', JSON.stringify(token))
-    //                     navigate("/main")
-    //                 })
-    //                 .catch((error) => {
-    //                     // Error retrieving the token
-    //                     console.error('Error retrieving user token:', error);
-    //                 });
-    //         })
-    //         .catch((error) => {
-    //             // Error signing in anonymously
-    //             console.error('Error signing in anonymously:', error);
-    //         });
-    // }
-
     return (
         <div className="form-container">
             <Toaster />
@@ -139,9 +110,9 @@ const Login = () => {
                         onClick={togglePasswordVisibility}
                     />
                 </div>
-                <NavLink 
-                className='forgot-password'
-                to="/forgot-password"
+                <NavLink
+                    className='forgot-password'
+                    to="/forgot-password"
                 >Forgot password?</NavLink>
                 <button
                     className="form-submit"
